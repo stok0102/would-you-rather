b83b12e728c9cca92ec00da73a303a4962a63b32
Caleb Stokka and Noah Ramey <noah@>
2016-09-19 16:31:18 -0700

    add category to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919175522) do
+ActiveRecord::Schema.define(version: 20160919231253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160919175522) do
     t.string   "option_two"
     t.integer  "option_one_votes"
     t.integer  "option_two_votes"
+    t.string   "category"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 question_list = [
-  ["be without elbows", "be without knees"],
-  ["have to sneeze but not be able to", "have something stuck in your eye for a year"],
-  ["have a head the size of a tennis ball", "have a head the size of a watermelon"],
-  ["live forever", "die in the next five minutes"],
-  ["have one get out of jail free card", "have a key that opens any door"],
-  ["be forced to dance to any song you hear", "sing aloud every song you hear"]
+  ["be without elbows", "be without knees", "body horror"],
+  ["have to sneeze but not be able to", "have something stuck in your eye for a year", "body horror"],
+  ["have a head the size of a tennis ball", "have a head the size of a watermelon", "body horror"],
+  ["live forever", "die in the next five minutes", "existential"],
+  ["have one get out of jail free card", "have a key that opens any door", "genie"],
+  ["be forced to dance to any song you hear", "sing aloud every song you hear", "torture"]
 ]
 
-question_list.each do |a, b|
-  Question.create( option_one: a, option_two: b, option_one_votes: 0, option_two_votes: 0)
+question_list.each do |a, b, c|
+  Question.create( option_one: a, option_two: b, option_one_votes: 0, option_two_votes: 0, category: c)
 end
